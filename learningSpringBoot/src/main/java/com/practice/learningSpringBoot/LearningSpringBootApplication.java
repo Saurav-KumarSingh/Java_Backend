ee7449f255c10c9bdb20023c2e4a21acfe79bf38
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-09-30 10:56:58 +0530

    h2 in memory db

--- learningSpringBoot/src/main/java/com/practice/learningSpringBoot/LearningSpringBootApplication.java
+++ learningSpringBoot/src/main/java/com/practice/learningSpringBoot/LearningSpringBootApplication.java
@@ -1,13 +1,33 @@
 package com.practice.learningSpringBoot;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class LearningSpringBootApplication {
+@Log
+public class LearningSpringBootApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    private  final DataSource dataSource;
+
+    public LearningSpringBootApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBootApplication.class, args);
 	}
+
+    @Override
+    public void run(final String...args) {
+        log.info("Data Source: " + dataSource.toString());
+        final JdbcTemplate restTemplate=new JdbcTemplate(dataSource);
+        restTemplate.execute("select 1");
+
+    }
 
 }
