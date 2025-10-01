e04df802125b88f05ba1faa9558d04ce5d210057
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-10-01 16:53:45 +0530

    crud

--- learningSpringBoot/src/main/java/com/practice/learningSpringBoot/controller/DepartmentController.java
+++ learningSpringBoot/src/main/java/com/practice/learningSpringBoot/controller/DepartmentController.java
@@ -4,10 +4,7 @@ package com.practice.learningSpringBoot.controller;
 import com.practice.learningSpringBoot.entity.Department;
 import com.practice.learningSpringBoot.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,21 @@ public class DepartmentController {
     @GetMapping("/department")
     public List<Department> fetchDepartmentList(){
         return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/department/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/department/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Deleted Succesfully !";
+    }
+
+    @PutMapping("/department/{id}")
+    public Department updateDepartmentById(@PathVariable("id") Long departmentId,@RequestBody Department department){
+        return departmentService.updateDepartmentById(departmentId,department);
     }
 }
--- learningSpringBoot/src/main/java/com/practice/learningSpringBoot/service/DepartmentServiceImpl.java
+++ learningSpringBoot/src/main/java/com/practice/learningSpringBoot/service/DepartmentServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
@@ -23,4 +24,38 @@ public class DepartmentServiceImpl implements DepartmentService{
     public List<Department> fetchDepartmentList() {
         return departmentRepository.findAll();
     }
+
+    @Override
+    public Department fetchDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId).get();
+    }
+
+    @Override
+    public void deleteDepartmentById(Long departmentId) {
+        departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartmentById(Long departmentId, Department department) {
+        Department depDb = departmentRepository.findById(departmentId)
+                .orElseThrow(() -> new RuntimeException("Department not found with id: " + departmentId));
+
+        if (Objects.nonNull(department.getDepartmentName()) &&
+                !"".equalsIgnoreCase(department.getDepartmentName())) {
+            depDb.setDepartmentName(department.getDepartmentName());
+        }
+
+        if (Objects.nonNull(department.getDepartmentAddress()) &&
+                !"".equalsIgnoreCase(department.getDepartmentAddress())) {
+            depDb.setDepartmentAddress(department.getDepartmentAddress());
+        }
+
+        if (Objects.nonNull(department.getDepartmentCode()) &&
+                !"".equalsIgnoreCase(department.getDepartmentCode())) {
+            depDb.setDepartmentCode(department.getDepartmentCode());
+        }
+
+        return departmentRepository.save(depDb);
+    }
+
 }
