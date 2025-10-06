a60d0a4e0afe8eeb6b739fb6fdf1965c7ae0a682
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-10-06 11:21:31 +0530

    jpa annotations and props

--- spring-data-jpa/src/main/java/com/example/spring/data/jpa/entity/Student.java
+++ spring-data-jpa/src/main/java/com/example/spring/data/jpa/entity/Student.java
@@ -1,8 +1,7 @@
 package com.example.spring.data.jpa.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,14 +12,36 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
+@Table(name = "student_tbl",
+        uniqueConstraints = @UniqueConstraint(
+                name = "email_unique",
+                columnNames = "email"
+        )//email column must be unique
+)  //to create table as you want
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name ="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(name = "email",
+            nullable = false, //required field
+            unique = true
+    )  // to create column name as you want
     private String emailId;
     private String guardianName;
+
     private String guardianEmail;
     private String guardianMobile;
 }
