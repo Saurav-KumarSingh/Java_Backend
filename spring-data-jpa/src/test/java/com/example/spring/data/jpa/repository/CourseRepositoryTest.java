536adeffc06b6c35074f1b6da2f19c945ec5c26d
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-10-06 17:33:43 +0530

    One to One relationship

---
+++ spring-data-jpa/src/test/java/com/example/spring/data/jpa/repository/CourseRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.spring.data.jpa.repository;
+
+import com.example.spring.data.jpa.entity.Course;
+import com.example.spring.data.jpa.entity.CourseMaterial;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void printAllCourses(){
+        List<Course> courseList=courseRepository.findAll();
+
+        System.out.println(courseList);
+    }
+
+}
